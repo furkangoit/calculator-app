82cebaad6b5bc6d525dec7327c07d12f9f7e2b86
furkangoit <celikfurkan45@hotmail.com>
2025-05-01 14:45:24 +0300

    fixed

--- script.js
+++ script.js
@@ -5,17 +5,32 @@ let string = "";
 let arr = Array.from(buttons);
 arr.forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (e.target.innerHTML == "=") {
-      string = eval(string);
-      input.value = string;
-    } else if (e.target.innerHTML == "AC") {
+    let value = e.target.innerText;
+
+    if (value === "=") {
+      try {
+        string = eval(string);
+        input.value = string;
+      } catch {
+        input.value = "Error";
+        string = "";
+      }
+    } else if (value === "AC") {
       string = "";
       input.value = string;
-    } else if (e.target.innerHTML == "DEL") {
+    } else if (value === "DEL") {
       string = string.substring(0, string.length - 1);
       input.value = string;
+    } else if (value === "%") {
+      try {
+        string = (eval(string) / 100).toString();
+        input.value = string;
+      } catch {
+        input.value = "Error";
+        string = "";
+      }
     } else {
-      string += e.target.innerHTML;
+      string += value;
       input.value = string;
     }
   });
